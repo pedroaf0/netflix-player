7b6e751e82f80c1efab9c5d847d438f3f448deb8
pedroaf0 <pedroaf1233@gmail.com>
2020-01-27 13:40:27 -0300

    push

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -13,9 +13,7 @@ self.addEventListener("install", function (event) {
     caches.open(CACHE).then(function (cache) {
       console.log("[PWA Builder] Cached offline page during install");
 
-      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
-        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-      }
+      
 
       return cache.add(offlineFallbackPage);
     })
