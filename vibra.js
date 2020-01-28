b742463ef4d025f1af6b5fb3ef0e94ee27350ebd
pedroaf0 <pedroaf1233@gmail.com>
2020-01-27 23:10:14 -0300

    Update vibra.js

--- vibra.js
+++ vibra.js
@@ -1,17 +1,17 @@
 function vibra(){
     switch(video.currentTime) {
         case 20:
-            console.log("vibra 20");
             window.navigator.vibrate(200);
+            console.log("vibra 20");
             navigator.vibrate(200);
             navigator.vibrate(500);
 
           break;
         case 30:
+            navigator.vibrate(500);
           console.log("vibra 30");
           window.navigator.vibrate(200);
           navigator.vibrate(200);
-          navigator.vibrate(500);
 
 
           break;
