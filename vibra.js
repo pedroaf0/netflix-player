bdb6252d3a85b14562095f4a2cf091282db83b79
pedroaf0 <pedroaf1233@gmail.com>
2020-01-27 23:08:57 -0300

    Update vibra.js

--- vibra.js
+++ vibra.js
@@ -4,11 +4,15 @@ function vibra(){
             console.log("vibra 20");
             window.navigator.vibrate(200);
             navigator.vibrate(200);
+            navigator.vibrate(500);
+
           break;
         case 30:
           console.log("vibra 30");
           window.navigator.vibrate(200);
           navigator.vibrate(200);
+          navigator.vibrate(500);
+
 
           break;
         default:
